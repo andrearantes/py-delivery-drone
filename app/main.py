9f0a6e34807d50f711baa70f2a768482a108c97a
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-07 12:20:19 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -61,8 +61,8 @@ class DeliveryDrone(FlyingRobot):
 
     def hook_load(self, cargo: Cargo) -> None:
         self.cargo: Cargo
-        if (self.current_load is None and cargo.weight <=
-                self.max_load_weight):
+        if (self.current_load is None and cargo.weight
+                <= self.max_load_weight):
             self.current_load = cargo
 
     def unhook_load(self) -> None:
